3b398da1ad2d7536158dc7c615383e5690141884
jamesm@evergrowth.ca <jamesm@evergrowth.ca>
2025-06-02 13:15:18 -0600

    Commit build script and remove dead soul from test variant

---
+++ bc-tools-extension/_common/_build.ps1
@@ -0,0 +1,37 @@
+# this is required to copy the contents of the _common directory to the various subtask folders
+
+###############################################################################################
+# copy _common to all subfolders where _common is required
+###############################################################################################
+
+Set-Location -Path ".."
+
+Write-Host "Current working directory: $(Get-Location)"
+
+Write-Host "Copying _common contents to:"
+
+$paths = @(
+    "./Get-ListOfCompanies",
+    "./Get-ListOfModules",
+    "./Publish-BCModuleToTenant"
+)
+
+foreach($path in $paths) {
+    Write-Host ("  {0,20}" -f $path)
+    $destPath = Join-Path -Path $path -ChildPath "_common"
+    
+    Write-Host "DestPath: $destPath"
+    if (-not (Test-Path -Path $destPath)) {
+        New-Item -ItemType Directory -Path $destPath | Out-Null
+        Write-Host "Created path: $destPath"
+    }
+    #Copy-Item -Path "_common\*" -Destination "$destPath" -Recurse -Force
+    Get-ChildItem -Path "_common" | Where-Object { $_.Name -ne "_build.ps1" } | ForEach-Object {
+        Copy-Item -Path $_.FullName -Destination $destPath -Recurse -Force
+    }
+    Write-Host "Copied '_common' to $path"
+}
+
+Write-Host "Copy complete"
+
+###############################################################################################
