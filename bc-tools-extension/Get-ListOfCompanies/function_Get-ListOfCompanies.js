ce6c402ca79add725d47fec08abc229f5ad68833
jamesm@evergrowth.ca <jamesm@evergrowth.ca>
2025-06-02 10:13:26 -0600

    I NEED to see the internals of this

--- bc-tools-extension/Get-ListOfCompanies/function_Get-ListOfCompanies.js
+++ bc-tools-extension/Get-ListOfCompanies/function_Get-ListOfCompanies.js
@@ -1,16 +1,5 @@
 const fs = require('fs');
 
-fs.readdir('..', (err, files) => {
-    if (err) {
-        console.error('Error reading directory: ', err);
-        return;
-    }
-    console.log('Files in parent directory:');
-    files.forEach(file=> {
-        console.log(file);
-    });
-});
-
 const commonTools = require('_common/CommonTools');
 
 const tenantId = process.env.INPUT_TENANTID;
@@ -19,18 +8,28 @@ const clientSecret = process.env.INPUT_CLIENTSECRET;
 const environmentName = process.env.INPUT_ENVIRONMENTNAME;
 
 (async () => {
-    try {
-        const token = await commonTools.getToken(tenantId, clientId, clientSecret);
-        const companies = await commonTools.getCompanies(token, tenantId, environmentName);
-
-        console.log('Companies:');
-        companies.forEach((company, idx) => {
-            const name = company.name;
-            const id = company.id;
-            console.log(`${idx + 1}. ${company.name} (ID: ${company.id})`);
+    fs.readdir('..', (err, files) => {
+        if (err) {
+            console.error('Error reading directory: ', err);
+            return;
+        }
+        console.log('Files in parent directory:');
+        files.forEach(file => {
+            console.log(file);
         });
-    }
-    catch (error) {
-        console.error('Error: ', error.message);
-    }
+    });
+    // try {
+    //     const token = await commonTools.getToken(tenantId, clientId, clientSecret);
+    //     const companies = await commonTools.getCompanies(token, tenantId, environmentName);
+
+    //     console.log('Companies:');
+    //     companies.forEach((company, idx) => {
+    //         const name = company.name;
+    //         const id = company.id;
+    //         console.log(`${idx + 1}. ${company.name} (ID: ${company.id})`);
+    //     });
+    // }
+    // catch (error) {
+    //     console.error('Error: ', error.message);
+    // }
 })();
